f51d929f98580b45defd88b6c2e06d71d1f0af02
junehosoft <junehopark98@gmail.com>
2021-04-07 22:02:39 -0700

    Added linting config

---
+++ .prettierrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  bracketSpacing: true,
+  endOfLine: 'lf',
+  jsxBracketSameLine: false,
+  jsxSingleQuote: false,
+  quoteProps: 'as-needed',
+  semi: true,
+  trailingComma: 'all',
+  singleQuote: true,
+  printWidth: 80,
+  tabWidth: 2,
+  useTabs: false,
+};
